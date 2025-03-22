0945176b66baa4873566592887aec15bde755d9e
Reiad <reiadwc@gmail.com>
2025-03-21 22:34:23 -0400

    Updated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const { getCollection } = require('../models/db');
+const { connectToDB, getCollection } = require('../models/db');  
 
 const transport = nodemailer.createTransport({
   service: 'gmail',
@@ -10,43 +10,44 @@ const transport = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
   },
-})
+});
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Party Rentals', confirmation: req.query.confirmation || null }); 
+  res.render('index', { title: 'Party Rentals', confirmation: req.query.confirmation || null });
 });
 
 router.post('/submit', async (req, res) => {
   const { name, email, message } = req.body;
 
   try {
+
+    const db = await connectToDB();
     const contactsCollection = getCollection('contacts');
-    const newContact = {
-      name: name,
-      email: email,
-      message: message
-    };
+    
+
+    const newContact = { name, email, message };
+
 
     await contactsCollection.insertOne(newContact);
+
 
     const mailnotif = {
       from: process.env.EMAIL_USER,
       to: process.env.NOTIFY_EMAIL,
       subject: "Contact Form Submission",
-      text: 
-      `Contact Form Submission
-      Name: ${name},
-      Email: ${email},
-      Message: ${message}`,
-    }
+      text: `Contact Form Submission\nName: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    };
 
-    transport.sendMail(mailnotif, (error,info) => {
-      if(error){
-        console.error("Error sending email: ", error)
-      } else{
-        console.log("Email sent: " + info.response)
+
+    transport.sendMail(mailnotif, (error, info) => {
+      if (error) {
+        console.error("Error sending email: ", error);
+        res.status(500).send('There was an error sending the email.');
+        return;
       }
-    })
+      console.log("Email sent: " + info.response);
+    });
+
 
     res.redirect('/?confirmation=true');
   } catch (error) {
