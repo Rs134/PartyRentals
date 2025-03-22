bd3fc6320fe02fecda6b4b84a7160fa40fd4eda3
Reiad <reiadwc@gmail.com>
2025-03-21 21:52:39 -0400

    Database updated

--- models/db.js
+++ models/db.js
@@ -1,28 +1,33 @@
 const { MongoClient } = require('mongodb');
 const dbURL = process.env.ATLAS_URI;
 
+let client;
 let db;
 
 async function connectToDB() {
+  if (db) {
+    console.log("Using existing database connection");
+    return db;
+  }
+
   try {
-    const client = new MongoClient(dbURL); 
+    console.log("Connecting to MongoDB...");
+    client = new MongoClient(dbURL);
     await client.connect();
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
     db = client.db("Party_Rentals");
+    return db;
   } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+    console.error("Error connecting to MongoDB:", error);
     throw error;
   }
 }
 
 function getCollection(collectionName) {
   if (!db) {
-    throw new Error('Database connection not established. Call connectToDB first.');
+    throw new Error("Database connection not established. Call connectToDB first.");
   }
   return db.collection(collectionName);
 }
 
-module.exports = {
-  connectToDB,
-  getCollection,
-};
+module.exports = { connectToDB, getCollection };
